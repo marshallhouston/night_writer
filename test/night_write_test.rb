99fe195f46a3f75ce7b1cde3bb87d339c937accd
Marshall Houston <marshall.houston@gmail.com>
2017-08-31 08:31:58 -0600

    updates night_write_tests

--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -7,26 +7,20 @@ class NightWriteTest < Minitest::Test
 
   def test_night_write_class_exists
     night_write = NightWrite.new
+
     assert_instance_of NightWrite, night_write
   end
 
-  def test_opens_and_reads_information_in_ARGV0_using_FileReader
-    night_write = NightWrite.new
+  def test_can_create_new_instance_of_translator
+    NightWrite.new
 
-    assert_instance_of FileReader, night_write.reader
+    assert_instance_of Translator, Translator.new
   end
 
-  def test_passes_information_from_FileReader_to_Translator
-    night_write = NightWrite.new
+  def test_can_create_new_instance_of_printer
+    NightWrite.new
 
-    assert_instance_of Translator, night_write.translator
-  end
-
-  def test_passes_translated_information_to_Printer
-    night_write = NightWrite.new
-
-    assert_instance_of Printer, night_write.printer
-
+    assert_instance_of Printer, Printer.new
   end
 
 end
