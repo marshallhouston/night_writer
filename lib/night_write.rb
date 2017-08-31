0af079d9ef51b4dbd19635d74cd0f4f8178eee67
Marshall Houston <marshall.houston@gmail.com>
2017-08-30 18:46:57 -0600

    removes unused files

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,24 +1,19 @@
-require './lib/file_reader'
+# require './lib/file_reader'
 require './lib/translator'
 require './lib/printer'
 
 class NightWrite
 
   def initialize
-
-    input_file_name = ARGV[0]
-
-    output_file_name = ARGV[1]
-
     lines_of_text = File.open(ARGV[0])
-    text_to_translate = FileReader.new.read(lines_of_text)
+    text_to_translate = File.read(lines_of_text)
 
     braille_characters = Translator.new.translate_to_braille(text_to_translate)
     final_text = Printer.new.print(braille_characters)
 
-    File.write(output_file_name, final_text)
+    File.write(ARGV[1], final_text)
 
-    puts "Created '#{output_file_name}' containing #{text_to_translate.strip.length} characters"
+    puts "Created '#{ARGV[1]}' containing #{text_to_translate.strip.length} characters"
   end
 end
 
